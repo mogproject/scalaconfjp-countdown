bb0b121489300af72a241cfb5cbc70f7c9ab1a33
mogproject <mogproj@gmail.com>
2012-10-26 17:03:14 +0900

    Get environment variables

--- src/main/scala/ScalaConfJPCountDown.scala
+++ src/main/scala/ScalaConfJPCountDown.scala
@@ -1,12 +1,12 @@
-import scalaj.http.HttpOptions._
 import scalaj.http.{Token, Http}
+import scalaj.http.HttpOptions._
 import java.util.Calendar
 
 object ScalaConfJPCountDown extends App {
   def tweet(msg: String) {
     if (msg.isEmpty) return
-    val consumer = Token("BK5XIfKA8pmO9AaQdOkcQ", "3bzIaiXHjFFrwz9n3r5dqUiCCMabievCsMnS2TPFPw")
-    val accessToken = Token("903128250-uzeKqQcrxbL1P9XaPEZOeYgN8j9JHx3zSif0SYg6", "ABiXSwDnj6e5mMjlj8T0lqr0oZ8NZUxHfx58SRb48eE")
+    val consumer = Token(System.getenv("CONSUMER_KEY"), System.getenv("CONSUMER_SECRET"))
+    val accessToken = Token(System.getenv("ACCESS_TOKEN"), System.getenv("ACCESS_SECRET"))
     Http.post("https://api.twitter.com/1/statuses/update.xml")
       .options(connTimeout(1000), readTimeout(5000))
       .params("status" -> msg)
@@ -16,6 +16,7 @@ object ScalaConfJPCountDown extends App {
 
   def createMessage(dateDiff: Long): String = {
     val event = "Scala Conference in Japan"
+    val foot = " http://www.scalaconf.jp"
     val body = dateDiff match {
       case -2 => "本サービスは本日をもって終了いたします。ご利用ありがとうございました。"
       case -1 => event + " 本日はアンカンファレンス開催日です!"
@@ -24,13 +25,12 @@ object ScalaConfJPCountDown extends App {
       case days if days > 0 => event + " 開催まであと " + days + "日です!"
       case _ => return ""
     }
-    val tail = " http://www.scalaconf.jp"
-    body + tail
+    body + foot
   }
 
   val now = Calendar.getInstance
   val cal = now.clone.asInstanceOf[Calendar]
-  cal.set(2013, 2, 2)
+  cal.set(2013, 2, 2)  // 2013-03-02
   val diff = (cal.getTime.getTime - now.getTime.getTime) / 24 / 60 / 60 / 1000
   tweet(createMessage(diff))
 }
